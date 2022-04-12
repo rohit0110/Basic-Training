6be5445b659ef88d30a42f0b28bb42d6e1c2057f
rohit0110 <rohit7rathore15@gmail.com>
2022-04-12 21:35:03 +0530

    Build different notifications

--- aesthetic/lib/notifications/notifs.dart
+++ aesthetic/lib/notifications/notifs.dart
@@ -35,6 +35,68 @@ class _NotifsState extends State<Notifs> {
                     ScaffoldMessenger.of(context).showSnackBar(snackBar);
                   },
                   child: const Text("Show Snackbar")),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white)),
+                  onPressed: () {
+                    showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                              //shape: const CircleBorder(side: BorderSide.none),
+
+                              title: const Text("Alert Dialoge button"),
+                              content: SingleChildScrollView(
+                                child: ListBody(children: const [
+                                  Text("Just a demo alert dialog"),
+                                  Text("Approve message?")
+                                ]),
+                              ),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text("Approve"))
+                              ],
+                              actionsAlignment: MainAxisAlignment.center,
+                              backgroundColor: Colors.amber,
+                            ));
+                  },
+                  child: const Text("Show AlertDialog")),
+              const SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white)),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) => SimpleDialog(
+                              backgroundColor: Colors.amber,
+                              contentPadding: const EdgeInsets.all(20),
+                              title: const Text('Select one option'),
+                              children: [
+                                SimpleDialogOption(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text("option 1"),
+                                ),
+                                SimpleDialogOption(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text("option 2"),
+                                ),
+                              ],
+                            ));
+                  },
+                  child: const Text("Show SimpleDialog")),
             ],
           ),
         ),
